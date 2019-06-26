33d83f974e9d73c7e3d058612dbaedafb8c57557
kadoshita <sublimer.me@gmail.com>
2019-06-26 11:37:56 +0900

    close #1

--- CPUTemperature/Program.cs
+++ CPUTemperature/Program.cs
@@ -12,15 +12,32 @@ namespace CPUTemperature
             var cpus = Cpu.Discover();
 
             int corecount, cpucount = 0;
-            foreach (var cpu in cpus)
+            if (args[0] == "telegraf")
             {
-                corecount = 0;
-                foreach (var core in cpu.CoreTemperatures)
+                foreach (var cpu in cpus)
                 {
-                    Console.WriteLine($"{measurement},cpu={cpucount},core={corecount} max={core.Max},min={core.Min},value={core.Value}");
-                    corecount++;
+                    corecount = 0;
+                    foreach (var core in cpu.CoreTemperatures)
+                    {
+                        Console.WriteLine($"{measurement},cpu={cpucount},core={corecount} max={core.Max},min={core.Min},value={core.Value}");
+                        corecount++;
+                    }
+                    cpucount++;
                 }
-                cpucount++;
+            }
+            else if (args[0] == "mackerel")
+            {
+                var datetime = DateTime.Now.ToUniversalTime();
+                var timestamp = (long)datetime.Subtract(datetimeUnixEpoch).TotalSeconds;
+                corecount = 0;
+                foreach (var cpu in cpus)
+                {
+                    foreach (var core in cpu.CoreTemperatures)
+                    {
+                        Console.WriteLine($"temp.core{corecount}\t{core.Value}\t{timestamp}");
+                        corecount++;
+                    }
+                }
             }
         }
     }
